c89fefcce1ce3e79d2b3725358db9239ed1068b6
Jordan Brantner <thejordanbrantner@gmail.com>
2023-09-22 10:33:48 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
+let employee = {
+    name: "john",
+    streetAddress: "something",
+}
 
 
-const employee = {
-        name: " ",
-        streetAddress: " ",
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
     }
-
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-        const employee2 = {...employee};
-        employee2[key] = value;
-        return employee2;
-};
+    
+}
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
 }
+function deleteFromEmployeeByKey(employee, key) {
+let deleteFromEmployeeByKey = {...employee}
 
-function deleteFromEmployeeByKey(employee, key, value) {
-    const employee3 = {...employee};
-    employee3[key] = value;
-    return employee3;
+delete deleteFromEmployeeByKey[key];
+
+return deleteFromEmployeeByKey;
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
-    employee[key] = value;
-    return employee;
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+delete employee[key];
+return employee;
 }
